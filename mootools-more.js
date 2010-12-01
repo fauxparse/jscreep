48beb904d35dd5bdaca50c6abd291209a8d0fe29
Matt Powell <fauxparse@gmail.com>
2010-12-01 17:22:44 +1300

    start/pause

--- mootools-more.js
+++ mootools-more.js
@@ -1,6 +1,6 @@
 // MooTools: the javascript framework.
-// Load this file's selection again by visiting: http://mootools.net/more/d00206d2df9cf5e09887e32e503767c0 
-// Or build this file again with packager using: packager build More/More More/Events.Pseudos More/Class.Occlude More/Chain.Wait More/Array.Extras More/Object.Extras More/String.Extras More/Element.Event.Pseudos More/Element.Delegation More/Drag More/Drag.Move More/Tips
+// Load this file's selection again by visiting: http://mootools.net/more/efdbbf406c89a5f02ce1a8e20717eb97 
+// Or build this file again with packager using: packager build More/More More/Events.Pseudos More/Class.Occlude More/Chain.Wait More/Array.Extras More/Object.Extras More/String.Extras More/Element.Event.Pseudos More/Element.Delegation More/Element.Shortcuts More/Drag More/Drag.Move More/Tips
 /*
 ---
 
@@ -639,6 +639,81 @@ Event.definePseudo('relay', function(split, fn, args, proxy){
 		base: 'mouseout',
 		condition: Element.Events.mouseleave.condition
 	}
+});
+
+
+/*
+---
+
+script: Element.Shortcuts.js
+
+name: Element.Shortcuts
+
+description: Extends the Element native object to include some shortcut methods.
+
+license: MIT-style license
+
+authors:
+  - Aaron Newton
+
+requires:
+  - Core/Element.Style
+  - /MooTools.More
+
+provides: [Element.Shortcuts]
+
+...
+*/
+
+Element.implement({
+
+	isDisplayed: function(){
+		return this.getStyle('display') != 'none';
+	},
+
+	isVisible: function(){
+		var w = this.offsetWidth,
+			h = this.offsetHeight;
+		return (w == 0 && h == 0) ? false : (w > 0 && h > 0) ? true : this.style.display != 'none';
+	},
+
+	toggle: function(){
+		return this[this.isDisplayed() ? 'hide' : 'show']();
+	},
+
+	hide: function(){
+		var d;
+		try {
+			//IE fails here if the element is not in the dom
+			d = this.getStyle('display');
+		} catch(e){}
+		if (d == 'none') return this;
+		return this.store('element:_originalDisplay', d || '').setStyle('display', 'none');
+	},
+
+	show: function(display){
+		if (!display && this.isDisplayed()) return this;
+		display = display || this.retrieve('element:_originalDisplay') || 'block';
+		return this.setStyle('display', (display == 'none') ? 'block' : display);
+	},
+
+	swapClass: function(remove, add){
+		return this.removeClass(remove).addClass(add);
+	}
+
+});
+
+Document.implement({
+
+	clearSelection: function(){
+		if (document.selection && document.selection.empty){
+			document.selection.empty();
+		} else if (window.getSelection){
+			var selection = window.getSelection();
+			if (selection && selection.removeAllRanges) selection.removeAllRanges();
+		}
+	}
+
 });
 
 
